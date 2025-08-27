b54fd36744ec156eea6b2026f9bf863e582c346b
Back370 <y.endo.203@stn.nitech.ac.jp>
2025-08-27 19:38:14 +0900

    途中経過

--- app/src/main/java/com/example/chap/MainActivity.kt
+++ app/src/main/java/com/example/chap/MainActivity.kt
@@ -6,7 +6,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Modifier
 import com.example.chap.API.PostViewModel
 import com.example.chap.ui.theme.CHAPTheme
@@ -20,11 +28,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         PostViewModel.getAllPosts()
         setContent {
-            CHAPTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
+            Scaffold(
+                bottomBar = {
+                    BottomAppBar(
+                        actions = {
+                            IconButton(onClick = { /* TODO */ }) {
+                                Icon(painterResource(id = R.drawable.outline_home_24), contentDescription = "Home")
+                            }
+                            IconButton(onClick = { /* TODO */ }) {
+                                Icon(painterResource(id = R.drawable.outline_map_24), contentDescription = "Map")
+                            }
+                            IconButton(onClick = { /* TODO */ }) {
+                                Icon(painterResource(id = R.drawable.outline_flag_24), contentDescription = "Event")
+                            }
+                            IconButton(onClick = { /* TODO */ }) {
+                                Icon(painterResource(id = R.drawable.outline_mode_comment_24), contentDescription = "Thread")
+                            }
+                        }
+                    )
+                }
+            ) { innerPadding ->
+                // innerPadding を適用して画面本体を表示
+                Surface(modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()) {
                     Navigation()
                 }
             }
+//            CHAPTheme {
+//                Surface(modifier = Modifier.fillMaxSize()) {
+//                    Navigation()
+//                }
+//            }
         }
     }
 }
